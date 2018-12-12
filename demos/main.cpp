812c2eeac514f21792035d4163c28c8645872834
Ange Yaghi <Ange Yaghi>
2018-12-11 23:24:24 -0500

    [#48] : Added temporary fix which exits after minimum octree width is reached

--- demos/main.cpp
+++ demos/main.cpp
@@ -6,7 +6,7 @@ int main() {
 	//simpleRoomDemo(100, 1024*2, 768*2); //1024, 768
 	//boxCityDemo(1, 1024*2, 768*2);
 	//teapotDemo(1, 500, 500);
-	teapotLampDemo(641, 1024*2, 768*2); // 10 for issue replication 1024, 768
+	teapotLampDemo(1, 1024*2, 768*2); // 10 for issue replication 1024, 768
 	//cubeTestDemo(1, 1024, 768);
 	//blocksDemo(10, 1024 * 2, 768 * 2); // 10 for issue replication 1024, 768
 
