93016951e46eb36f591ff1d3d40cdfa9af90188b
Ange Yaghi <me@angeyaghi.com>
2019-02-01 19:29:36 -0500

    [#98] : Added material nodes to parameterize inputs

--- src/simple_lambert_material.cpp
+++ src/simple_lambert_material.cpp
@@ -49,9 +49,20 @@ void manta::SimpleLambertMaterial::generateRays(RayContainer *rays, const LightR
 	math::Vector m, o;
 	math::real weight;
 
-	m = m_diffuseBSDF.generateMicrosurfaceNormal(intersectionPoint.m_vertexNormal, incidentRay.getDirection(), u, v);
-	o = m_diffuseBSDF.reflectionDirection(incidentRay.getDirection(), m);
-	weight = m_diffuseBSDF.generateWeight(intersectionPoint.m_vertexNormal, incidentRay.getDirection(), m, o);
+	BSDFInput b_in;
+	b_in.incident = incidentRay.getDirection();
+	b_in.normal = intersectionPoint.m_vertexNormal;
+	b_in.surfaceInteraction = &intersectionPoint;
+	b_in.u = u;
+	b_in.v = v;
+
+	m_diffuseBSDF.initialize(&b_in, stackAllocator);
+
+	m = m_diffuseBSDF.generateMicrosurfaceNormal(b_in);
+	o = m_diffuseBSDF.reflectionDirection(b_in, m);
+	weight = m_diffuseBSDF.generateWeight(b_in, m, o);
+
+	m_diffuseBSDF.free(&b_in, stackAllocator);
 
 	constexpr math::real MAX_WEIGHT = (math::real)2.0;
 
---
+++ src/single_color_node.cpp
@@ -0,0 +1,17 @@
+#include <single_color_node.h>
+
+manta::SingleColorNode::SingleColorNode() {
+	m_color = math::constants::Zero;
+}
+
+manta::SingleColorNode::SingleColorNode(const math::Vector &color) {
+	m_color = color;
+}
+
+manta::SingleColorNode::~SingleColorNode() {
+
+}
+
+manta::math::Vector manta::SingleColorNode::sample(const IntersectionPoint *) const {
+	return m_color;
+}
